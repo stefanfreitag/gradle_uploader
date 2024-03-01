e547e42648ab27228534539b43b366ac6abd54f9
Stefan Freitag <stefan.freitag@rwe.com>
2024-03-01 06:19:11 +0100

    fix: updated repository url in projen configuration

--- .projenrc.js
+++ .projenrc.js
@@ -7,8 +7,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorName: 'Stefan Freitag',
   name: 'gradle_s3_uploader',
   description: 'Uploads new Gradle versions to an S3 bucket',
-  repository: 'https://github.com/stefanfreitag/projen_gradle_uploader.git',
-  cdkVersion: '2.103.1',
+  repository: 'https://github.com/stefanfreitag/gradle_uploader.git',
+  cdkVersion: '2.130.0',
   codeCov: true,
   defaultReleaseBranch: 'master',
   depsUpgradeOptions: {
